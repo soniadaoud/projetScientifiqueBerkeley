1c076c972f84676205ae9f3a201e4a476a417542
soniadaoud <sonia.daoud@hotmail.com>
2017-09-26 13:42:12 -0700

    ellipse inclinee

--- Artery-geometry-generation.py
+++ Artery-geometry-generation.py
@@ -2,10 +2,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
-
+#Parameters
 
 #Artery wall - Blue
-a_wall_in=%
+a_wall_in=5
 b_wall_in= 3
 thick_wall = 1.1
 
@@ -13,10 +13,19 @@ thick_wall = 1.1
 x_L= -1
 R_L = 2.2
 
-#Calcium/Lipid mass - Green
-x_M = 3
-a_M = 0.4
-b_M = 1.2
+#Lipid mass - Green
+x_lip = 1.5
+y_lip = 1.5
+a_lip = 0.2
+b_lip = 0.5
+alpha_lip = np.pi/4 #angle in rad
+
+#Calcium mass - Yellow
+x_cal = 2
+y_cal = -1.2
+a_cal = 0.3
+b_cal = 0.9
+alpha_cal = np.pi/3 #angle in rad
 #Graphs
 
 #Abscisse
@@ -40,15 +49,27 @@ def lumen(x_L,R_L):
     z_lumen = -np.sqrt(R_L**2 - (x-x_L)**2)
     return y_lumen,z_lumen
 
-#Calcium/lipid mass - ellipse centered on x_M with main axis a_M and small axis b_M
-def mass(x_M,a_M,b_M):
-    y_mass = +(b_M/a_M)*np.sqrt(a_M**2-(x-x_M)**2)
-    z_mass = -(b_M/a_M)*np.sqrt(a_M**2-(x-x_M)**2)
-    return y_mass,z_mass
+#Calcium mass - ellipse centered on x_cal,y_cal with main axis a_cal and small axis b_cal, angle of rotation alpha_cal
+def calcium(x_cal,y_cal,a_cal,b_cal,alpha_cal):
+    A = (b_cal**2)*(np.sin(alpha_cal)**2)+(a_cal**2)*(np.cos(alpha_cal)**2)
+    B = 2*(x-x_cal)*np.cos(alpha_cal)*np.sin(alpha_cal)*((b_cal**2)+(a_cal**2))
+    C = ((x-x_cal)**2)*((b_cal**2)*(np.cos(alpha_cal)**2)+(a_cal**2)*(np.sin(alpha_cal)**2))-(a_cal**2)*(b_cal**2)
+    y_calcium = y_cal + (-B+np.sqrt(-4*A*C))/(2*A)
+    z_calcium = y_cal + (-B-np.sqrt(-4*A*C))/(2*A)
+    return y_calcium,z_calcium
+
+#Lipid mass - ellipse centered on x_lip,y_lip with main axis a_lip and small axis b_lip, angle of rotation alpha_lip
+def lipid(x_lip,y_lip,a_lip,b_lip,alpha_lip):
+    A = (b_lip**2)*(np.sin(alpha_lip)**2)+(a_lip**2)*(np.cos(alpha_lip)**2)
+    B = 2*(x-x_lip)*np.cos(alpha_lip)*np.sin(alpha_lip)*((b_lip**2)+(a_lip**2))
+    C = ((x-x_lip)**2)*((b_lip**2)*(np.cos(alpha_lip)**2)+(a_lip**2)*(np.sin(alpha_lip)**2))-(a_lip**2)*(b_lip**2)
+    y_lipid = y_lip + (-B+np.sqrt(-4*A*C))/(2*A)
+    z_lipid = y_lip + (-B-np.sqrt(-4*A*C))/(2*A)
+    return y_lipid,z_lipid
 
 #Parameters Verification - avoid intersections
 
-def check_intersection(y_1,y_2):
+def check(y_1,y_2):
     intersect = np.argwhere(np.diff(np.sign(y_1 - y_2)) != 0).reshape(-1) + 0
     if (intersect!=[]):
         x_intersect = []
@@ -63,17 +84,13 @@ def check_intersection(y_1,y_2):
         if y_intersect != []:
             plt.plot(x_intersect, y_intersect, 'bo')
             return True
+#plots
+def artery_plot(a_wall_in, b_wall_in, thick_wall,
+                x_L, R_L,
+                x_cal, y_cal, a_cal, b_cal, alpha_cal,
+                x_lip, y_lip, a_lip, b_lip, alpha_lip):
 
-            #plots
-def artery_plot(a_wall_in,
-                b_wall_in,
-                thick_wall,
-                x_L,
-                R_L,
-                x_M,
-                a_M,
-                b_M):
-
+    plt.gcf().clear() #flush the previous plot if still loaded
 
     y_wall_in,z_wall_in,y_wall_out,z_wall_out = artery_wall(a_wall_in, b_wall_in, thick_wall)
     plt.plot(x,y_wall_in,'b-')
@@ -85,9 +102,13 @@ def artery_plot(a_wall_in,
     plt.plot(x,y_lumen,'r-')
     plt.plot(x,z_lumen,'r-')
 
-    y_mass,z_mass = mass(x_M,a_M,b_M)
-    plt.plot(x,y_mass,'g-')
-    plt.plot(x,z_mass,'g-')
+    y_calcium,z_calcium = calcium(x_cal,y_cal,a_cal,b_cal,alpha_cal)
+    plt.plot(x,y_calcium,'g-')
+    plt.plot(x,z_calcium,'g-')
+
+    y_lipid,z_lipid = lipid(x_lip,y_lip,a_lip,b_lip,alpha_lip)
+    plt.plot(x,y_lipid,'y-')
+    plt.plot(x,z_lipid,'y-')
 
     plot_limit = a_wall_in*thick_wall*1.1
     plt.xlim([-plot_limit,plot_limit])
@@ -96,21 +117,57 @@ def artery_plot(a_wall_in,
 
 
     #verifications
-    if check_intersection(y_lumen,y_wall_in):
-        red_patch = mpatches.Patch(color='red', label='crossing')
-        plt.legend(handles=[red_patch])
-    if check_intersection(y_mass,y_wall_in):
-        red_patch = mpatches.Patch(color='red', label='crossing')
-        plt.legend(handles=[red_patch])
-    if x_M-a_M<x_L+R_L:
-        red_patch = mpatches.Patch(color='red', label='crossing')
-        plt.legend(handles=[red_patch])
+#    if check(y_lumen,y_wall_in):
+        ##Uncomment two next lines if you want to plot the artery anyway with a red alert patch
+        #red_patch = mpatches.Patch(color='red', label='crossing')
+        #plt.legend(handles=[red_patch])
+#        return False #stop the process, don't plot. Comment if you want to see the plot anyway
+#    elif check(y_lipid,y_wall_in):
+#        return False
+#    elif check(y_lipid,y_lumen):
+#        return False
+#    elif check(y_lipid,y_calcium):
+#        return False
+#    elif check(y_calcium,y_wall_in):
+#        return False
+#    elif check(y_calcium,y_lumen):
+#        return False
+
+
+
 
     plt.show()
-    print("a_wall_in=%f, b_wall_in=%f, thick_wall=%f,x_L=%f, R_L=%f, x_M=%f,a_M=%f,b_M=%f"%(a_wall_in, b_wall_in, thick_wall,x_L, R_L, x_M,a_M,b_M))
-
+    #print("a_wall_in=%f, b_wall_in=%f, thick_wall=%f,x_L=%f, R_L=%f, x_M=%f,a_M=%f,b_M=%f"%(a_wall_in, b_wall_in, thick_wall,x_L, R_L, x_M,a_M,b_M))
+    import time
+count = 0
+t1 = time.time()
 for a_wall_in in [4,6]:
-    for R_L in [2.2,2.7]:
-        for x_M in [3,3.6]:
-            for x_L in [-1.2,-0.6]:
-                artery_plot(a_wall_in,b_wall_in,thick_wall,x_L,R_L,x_M,a_M,b_M)
+    for R_L in [1.2,1.7]:
+        for x_L in [-1.2,-0.6]:
+            plot = artery_plot(a_wall_in, b_wall_in, thick_wall,x_L, R_L,x_cal, y_cal, a_cal, b_cal, alpha_cal,x_lip, y_lip, a_lip, b_lip, alpha_lip)  #will plot only if there is no crossing add return +1 to count
+            if plot != False:
+                count +=1
+            print ("intersection: %r " % plot)
+print("Time to generate %d plots : %f seconds "%(count, time.time()-t1))
+
+f = np.linspace(-10,10,10000)
+alpha = np.pi/4
+a = 4
+b = 2
+for alpha in [0,np.pi/4,np.pi/3,np.pi/2]:
+    A = (b**2)*(np.sin(alpha)**2)+(a**2)*(np.cos(alpha)**2)
+    B = 2*t*np.cos(alpha)*np.sin(alpha)*((b**2)+(a**2))
+    C = (t**2)*((b**2)*(np.cos(alpha)**2)+(a**2)*(np.sin(alpha)**2))-(a**2)*(b**2)
+    y_plus = (-B+np.sqrt(-4*A*C))/(2*A)
+    y_neg = (-B-np.sqrt(-4*A*C))/(2*A)
+    plt.plot(f,y_neg)
+    plt.plot(f,y_plus)
+    plt.xlim([-a*2,a*2])
+    plt.ylim([-a*2,a*2])
+    plt.gca().set_aspect('equal', adjustable='box')
+    plt.show()
+
+    if (4==4 or 3==2):
+    print ('ok')
+
+artery_plot(a_wall_in, b_wall_in, thick_wall, x_L, R_L,x_cal, y_cal, a_cal,b_cal, alpha_cal,x_lip, y_lip, a_lip, b_lip, alpha_lip)
